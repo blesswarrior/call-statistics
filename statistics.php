2cd4bbf131e660ca98069a539dbcd11dce934a4f
blesswarrior <blesswarrior@qq.com>
2016-07-15 11:35:46 +0800

    自动生成通话时长报表

--- statistics.php
+++ statistics.php
@@ -1,5 +1,29 @@
 <?php
+require 'includes/start.php';
 
-if ($_GET['action'] == 'report') {
-    $_GET['']
+class Statistics extends Template
+{
+    
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    function index()
+    {
+        $cc_statistics = $this->medoo->get('cc_statistics', '*', 'ORDER BY `id` DESC');
+        dump('time:' . strftime($cc_statistics['createtime']));
+        dump(json_decode($cc_statistics['data']));
+    }
+
+    function report()
+    {
+        if (!$this->medoo->insert('cc_statistics', ['data' => $_POST['data']])) {
+            echo "error";
+        } else {
+            echo "success";
+        }
+    }
 }
+
+$c = new Statistics();
