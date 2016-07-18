526eff252747c1c1cbb11e8ebff80b1c0d367764
blesswarrior <blesswarrior@qq.com>
2016-07-18 09:57:22 +0800

    增加历史统计

--- statistics.php
+++ statistics.php
@@ -11,9 +11,14 @@ class Statistics extends Template
 
     function index()
     {
-        $result = $this->medoo->get('cc_statistics', '*', 'ORDER BY `date` DESC');
+        if (!$date = I('get.date', null)) {
+            $result = $this->medoo->get('cc_statistics', '*', ['ORDER' => ['id' => 'DESC']]);
+        } else {
+            $result = $this->medoo->get('cc_statistics', '*', ['date' => $date]);
+        }
         $this->assign('time', date('Y-m-d H:i:s', $result['updatetime']));
         $this->assign('list', json_decode($result['data']));
+        $this->assign('date', $this->medoo->select('cc_statistics', 'date'));
         $this->display();
     }
 
